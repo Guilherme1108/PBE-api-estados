ffcf22f5a8c88ee67d93b3adc73aae8e56220524
GUILHERME MOREIRA DE SOUZA <25133025@cfp127aluno.local>
2025-09-17 11:25:31 -0300

    fazendo a lógica de pesquisar por sigla

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const express    = require('express')     //Responsavel pela API
 const cors       = require('cors')        //Responsavel pelas permissões da API
 const bodyParser = require('body-parser') //Respnsavel por gerenciar a chegada dos dados da API com o front
 
+//Import do arquivo de funções
+const dados      = require('./modulo/funcoes.js')
+
 //Retorna a porta do servidor atual ou colocamos uma porta local
 const PORT = process.PORT || 8080
 
@@ -24,13 +27,61 @@ const app = express()
 //Configuração de permissões
 app.use((request, response, next) => {
     response.header('Acces-Control-Allow-Origin','*')    //Servidor de origem da API
-    response.header('Acces-Control-Allow-Methods','GET') //verbos permitidos
+    response.header('Acces-Control-Allow-Methods','GET') //verbos permitidos / para adicionar mais métodos separar por , dentro da mesma aspas
     //carrega as configurações no cors da API 
     app.use(cors())
     next() // proximo, carregar os proximos endpoints
 })
 
+//request  -> chegada de dados na API
+//response -> retorno de dados na API
+
 //ENDPOINTS
-app.get('/v1/estdos', function(request, response){
+app.get('/v1/estados', function(request, response){
+    //Pesquisa na função de estados  
+    let estados = dados.getAllEstados()
+
+    //Retrna o status code
+    response.status(estados.status_code)
     
+    //Retorna o JSON
+    response.json(estados)
+})
+
+app.get('/v1/estado/:uf', function(request, response){
+    let sigla = request.params.uf
+    let estado = dados.getEstadoBySigla(sigla)
+
+        //Retrna o status code
+        response.status(estado.status_code)
+    
+        //Retorna o JSON
+        response.json(estado)
+})
+
+app.get('/v1/estado/regiao/:uf'), function (request, response){
+    let sigla = request.params.uf
+    let estado = dados.getCapitalBySigla(sigla)
+
+            //Retrna o status code
+            response.status(estado.status_code)
+    
+            //Retorna o JSON
+            response.json(estado)
+}
+
+
+
+
+app.get('/v1/estados/regiao', function (request, response){
+    let sigla = request.query.uf
+    console.log(sigla)
+})
+
+
+
+
+//Start na API
+app.listen(PORT, function(){
+    console.log('API aguardando requisições ....')
 })
--- modulo/funcoes.js
+++ modulo/funcoes.js
@@ -5,20 +5,27 @@
  * Versão: 1.0
  ***************************************************************************************************************************/
 
-const MESSAGE_ERRO = {status : false, status_code : 500, development : 'Guilherme Moreira'}
+const MESSAGE_ERRO = { status: false, status_code: 500, development: 'Guilherme Moreira' }
+//Import do arquivode estados e cidades
 const dados = require('./estados_cidades.js')
 
 //Retorna todos os estados
-const getAllEstados = function(){
-    let message = {status : true, status_code : 200, development : 'Guilherme Moreira', uf : []}
+const getAllEstados = function () {
+    //Variavel de base para o cabeçalho da API
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', uf: [] }
 
-    dados.listaDeEstados.estados.forEach(function (item){
+    //Loop
+    dados.listaDeEstados.estados.forEach(function (item) {
         message.uf.push(item.sigla)
     })
+    //Para adiciona um elemento novo no json
     message.quantidade = message.uf.length //escrevendo o message.quantidade ele já irá criar o atributo quantidade
 
+    // //Para remover um atributo do JSON
+    // delete message.status
+
     // Se o if tiver mais de 1 coisa na resposta é necessario usar as {} já se for somente 1 resposta não são obrigatórias
-    if(message.uf.length > 0)
+    if (message.uf.length > 0)
         return message //Verdadeira 200
     else
         return MESSAGE_ERRO //Falsa 500
@@ -26,32 +33,68 @@ const getAllEstados = function(){
 }
 
 //Retorna um estado pesquisando pela sigla
-const getEstadoBySigla = function(sigla){
+const getEstadoBySigla = function (sigla) {
+    let siglaFormatada = String(sigla).toUpperCase()
+
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', estado: {} }
+
+    const estadoEncontrado = dados.listaDeEstados.estados.find(item => item.sigla === siglaFormatada)
+
+    if (estadoEncontrado) {
+        message.estado = {
+            uf: estadoEncontrado.sigla,
+            descricao: estadoEncontrado.nome,
+            capital: estadoEncontrado.capital,
+            regiao: estadoEncontrado.regiao
+        }
+        return message //Verdadeira 200
+    } else {
+        return MESSAGE_ERRO //Falsa 500
+    }
 
 }
 
 //Retorna a capital referente a um estado, pesquisando pela sigla
-const getCapitalBySigla = function(sigla){
+const getCapitalBySigla = function (sigla) {
 
+    let siglaFormatada = String(sigla).toUpperCase()
+
+    let message = { status: true, status_code: 200, development: 'Guilherme Moreira', capital: {} }
+
+    const estadoEncontrado = dados.listaDeEstados.estados.find(item => item.sigla === siglaFormatada)
+
+    if (estadoEncontrado) {
+        message.capital = {
+            uf: estadoEncontrado.sigla,
+            descricao: estadoEncontrado.nome,
+            capital: estadoEncontrado.capital
+        }
+        return message //Verdadeira 200
+    } else {
+        return MESSAGE_ERRO //Falsa 500
+    }
+    
 }
 
 //Retorna uma lista de estados pesqusiando pela região
-const getEstadosByRegiao = function(regiao){
+const getEstadosByRegiao = function (regiao) {
 
 }
 
 //Retorna uma lista de estados referente as capitias do país
-const getVerifyCapitaisDoPais = function(){
+const getVerifyCapitaisDoPais = function () {
 
 }
 
 //Retorna uma lista de cidades pesquisando pela sigla do estado
-const getCidadesBySigla = function(sigla){
+const getCidadesBySigla = function (sigla) {
 
 }
 
 // console.log(getAllEstados())
 
 module.exports = {
-    getAllEstados
+    getAllEstados,
+    getEstadoBySigla,
+    getCapitalBySigla
 }
